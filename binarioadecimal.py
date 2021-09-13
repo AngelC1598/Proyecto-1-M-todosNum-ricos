c51bbbe36441cfb8bebc75c804479a84a6ef5184
AngelC1598 <cantoral495@gmail.com>
2021-09-12 21:30:54 -0500

    Funcion binario a decimal

---
+++ binarioadecimal.py
@@ -0,0 +1,12 @@
+v = 0
+i = 0
+
+def binDec():
+    n=int(input("Ingrese el numero binario a transformar\n"))
+    print('El numero binario',n,end=' ')
+    while(n>=1):
+        d=n%10
+        n=int(n//10) #residuo de la division de n en 10
+        v=v+d*pow(2,i)
+        i=i+1
+    print(v, "en decimal")
